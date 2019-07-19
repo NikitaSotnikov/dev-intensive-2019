5ebad8cd2d4a9386274f455550bfe93869dd2883
Nikita Sotnikov <sotnikov.nikita@hotmail.com>
2019-07-19 23:57:10 +0300

    hometask_3 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,37 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.graphics.Rect
+
+
+fun Activity.hideKeyboard() {
+    if (currentFocus != null) {
+        val inputMethodManager = getSystemService(
+            Context
+                .INPUT_METHOD_SERVICE
+        ) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+    }
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val activityRootView = this.window.decorView
+    val r = Rect()
+    //r will be populated with the coordinates of your view that area still visible.
+    activityRootView.getWindowVisibleDisplayFrame(r)
+
+    val heightDiff = activityRootView.rootView.height - (r.bottom - r.top)
+    return heightDiff > 100
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    val activityRootView = this.window.decorView
+    val r = Rect()
+    //r will be populated with the coordinates of your view that area still visible.
+    activityRootView.getWindowVisibleDisplayFrame(r)
+
+    val heightDiff = activityRootView.rootView.height - (r.bottom - r.top)
+    return heightDiff < 100
+}
