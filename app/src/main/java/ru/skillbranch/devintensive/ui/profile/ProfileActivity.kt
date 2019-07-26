d363792c7449442aa4c70625aa073f11818f329c
NikitaSotnikov <sotnikov.nikita@hotmail.com>
2019-07-26 16:04:46 +0300

    - add new res - add map of view

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,20 +1,36 @@
 package ru.skillbranch.devintensive.ui.profile
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
 
 
 class ProfileActivity : AppCompatActivity(){
 
+    companion object{
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
+
+    var isEditMode = false
+    lateinit var viewFields: Map<String,TextView>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
+        initViews(savedInstanceState)
 
     }
 
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields= mapOf(
+                "nickname" to tv_nick_name,
+                "rank" to tv_rank,
+                "first_name" to et_first_name
+        )
+    }
 
 
     override fun onSaveInstanceState(outState: Bundle?) {
